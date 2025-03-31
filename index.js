99538df4e28971938faec1ff7dcb58a397f10fe2
michelle <michellemuchoki8@gmail.com>
2025-03-31 15:08:54 +0300

    objects2 done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+  name: "joy",
+  key: "streetAddress",
+};
+console.log(employee);
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return { ...employee, [key]: value };
+}
+const updatedEmployee = updateEmployeeWithKeyAndValue(employee, "place", thika);
+console.log(updatedEmployee);
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  
+  employee[key] = value;
+  return employee;
+}
+  
+function deleteFromEmployeeByKey(employee, key) {
+  const { [key]: _, ...rest } = employee;
+  return rest;
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
